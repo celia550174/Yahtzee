a5d6643c21271fdc121f5ba62da965da8bceeefb
celia <celia550174@yahoo.com.tw>
2023-10-13 11:24:33 +0800

    Initial Commit

---
+++ diceTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceTest
+//
+//  Created by celia on 2023/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
