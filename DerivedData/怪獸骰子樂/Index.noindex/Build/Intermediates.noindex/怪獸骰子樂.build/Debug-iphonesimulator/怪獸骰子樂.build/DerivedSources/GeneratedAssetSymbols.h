fb2d45b07e9a8fac7541245346a2206d8e120576
celia550174 <celia550174@yahoo.com.tw>
2023-11-30 10:31:37 +0800

    更新bonus分數問題

---
+++ DerivedData/怪獸骰子樂/Index.noindex/Build/Intermediates.noindex/怪獸骰子樂.build/Debug-iphonesimulator/怪獸骰子樂.build/DerivedSources/GeneratedAssetSymbols.h
@@ -0,0 +1,251 @@
+#import <Foundation/Foundation.h>
+
+#if __has_attribute(swift_private)
+#define AC_SWIFT_PRIVATE __attribute__((swift_private))
+#else
+#define AC_SWIFT_PRIVATE
+#endif
+
+/// The "3x" asset catalog image resource.
+static NSString * const ACImageName3X AC_SWIFT_PRIVATE = @"3x";
+
+/// The "4x" asset catalog image resource.
+static NSString * const ACImageName4X AC_SWIFT_PRIVATE = @"4x";
+
+/// The "Loading_blue" asset catalog image resource.
+static NSString * const ACImageNameLoadingBlue AC_SWIFT_PRIVATE = @"Loading_blue";
+
+/// The "Loading_light_y" asset catalog image resource.
+static NSString * const ACImageNameLoadingLightY AC_SWIFT_PRIVATE = @"Loading_light_y";
+
+/// The "Loading_orange" asset catalog image resource.
+static NSString * const ACImageNameLoadingOrange AC_SWIFT_PRIVATE = @"Loading_orange";
+
+/// The "Loading_text" asset catalog image resource.
+static NSString * const ACImageNameLoadingText AC_SWIFT_PRIVATE = @"Loading_text";
+
+/// The "O瞌睡蟲" asset catalog image resource.
+static NSString * const ACImageNameO AC_SWIFT_PRIVATE = @"O瞌睡蟲";
+
+#warning The "O萬事通" image asset name resolves to the symbol "ACImageNameO" which already exists. Try renaming the asset.
+
+#warning The "O開心鬼" image asset name resolves to the symbol "ACImageNameO" which already exists. Try renaming the asset.
+
+/// The "X瞌睡蟲" asset catalog image resource.
+static NSString * const ACImageNameX AC_SWIFT_PRIVATE = @"X瞌睡蟲";
+
+#warning The "X萬事通" image asset name resolves to the symbol "ACImageNameX" which already exists. Try renaming the asset.
+
+#warning The "X開心鬼" image asset name resolves to the symbol "ACImageNameX" which already exists. Try renaming the asset.
+
+/// The "Yahtzee" asset catalog image resource.
+static NSString * const ACImageNameYahtzee AC_SWIFT_PRIVATE = @"Yahtzee";
+
+/// The "again_off" asset catalog image resource.
+static NSString * const ACImageNameAgainOff AC_SWIFT_PRIVATE = @"again_off";
+
+/// The "again_on" asset catalog image resource.
+static NSString * const ACImageNameAgainOn AC_SWIFT_PRIVATE = @"again_on";
+
+/// The "arrow" asset catalog image resource.
+static NSString * const ACImageNameArrow AC_SWIFT_PRIVATE = @"arrow";
+
+/// The "award_1" asset catalog image resource.
+static NSString * const ACImageNameAward1 AC_SWIFT_PRIVATE = @"award_1";
+
+/// The "award_2" asset catalog image resource.
+static NSString * const ACImageNameAward2 AC_SWIFT_PRIVATE = @"award_2";
+
+/// The "award_3" asset catalog image resource.
+static NSString * const ACImageNameAward3 AC_SWIFT_PRIVATE = @"award_3";
+
+/// The "back_off" asset catalog image resource.
+static NSString * const ACImageNameBackOff AC_SWIFT_PRIVATE = @"back_off";
+
+/// The "back_on" asset catalog image resource.
+static NSString * const ACImageNameBackOn AC_SWIFT_PRIVATE = @"back_on";
+
+/// The "blue_1" asset catalog image resource.
+static NSString * const ACImageNameBlue1 AC_SWIFT_PRIVATE = @"blue_1";
+
+/// The "blue_2" asset catalog image resource.
+static NSString * const ACImageNameBlue2 AC_SWIFT_PRIVATE = @"blue_2";
+
+/// The "blue_3" asset catalog image resource.
+static NSString * const ACImageNameBlue3 AC_SWIFT_PRIVATE = @"blue_3";
+
+/// The "blue_4" asset catalog image resource.
+static NSString * const ACImageNameBlue4 AC_SWIFT_PRIVATE = @"blue_4";
+
+/// The "blue_5" asset catalog image resource.
+static NSString * const ACImageNameBlue5 AC_SWIFT_PRIVATE = @"blue_5";
+
+/// The "blue_6" asset catalog image resource.
+static NSString * const ACImageNameBlue6 AC_SWIFT_PRIVATE = @"blue_6";
+
+/// The "chance" asset catalog image resource.
+static NSString * const ACImageNameChance AC_SWIFT_PRIVATE = @"chance";
+
+/// The "choose_Single" asset catalog image resource.
+static NSString * const ACImageNameChooseSingle AC_SWIFT_PRIVATE = @"choose_Single";
+
+/// The "choose_double" asset catalog image resource.
+static NSString * const ACImageNameChooseDouble AC_SWIFT_PRIVATE = @"choose_double";
+
+/// The "dice" asset catalog image resource.
+static NSString * const ACImageNameDice AC_SWIFT_PRIVATE = @"dice";
+
+/// The "dice_off" asset catalog image resource.
+static NSString * const ACImageNameDiceOff AC_SWIFT_PRIVATE = @"dice_off";
+
+/// The "dice_on" asset catalog image resource.
+static NSString * const ACImageNameDiceOn AC_SWIFT_PRIVATE = @"dice_on";
+
+/// The "double_off" asset catalog image resource.
+static NSString * const ACImageNameDoubleOff AC_SWIFT_PRIVATE = @"double_off";
+
+/// The "double_on" asset catalog image resource.
+static NSString * const ACImageNameDoubleOn AC_SWIFT_PRIVATE = @"double_on";
+
+/// The "enter game button_off" asset catalog image resource.
+static NSString * const ACImageNameEnterGameButtonOff AC_SWIFT_PRIVATE = @"enter game button_off";
+
+/// The "enter game button_on" asset catalog image resource.
+static NSString * const ACImageNameEnterGameButtonOn AC_SWIFT_PRIVATE = @"enter game button_on";
+
+/// The "headshot_gray" asset catalog image resource.
+static NSString * const ACImageNameHeadshotGray AC_SWIFT_PRIVATE = @"headshot_gray";
+
+/// The "headshot_orange" asset catalog image resource.
+static NSString * const ACImageNameHeadshotOrange AC_SWIFT_PRIVATE = @"headshot_orange";
+
+/// The "home_blue" asset catalog image resource.
+static NSString * const ACImageNameHomeBlue AC_SWIFT_PRIVATE = @"home_blue";
+
+/// The "home_dice" asset catalog image resource.
+static NSString * const ACImageNameHomeDice AC_SWIFT_PRIVATE = @"home_dice";
+
+/// The "home_light" asset catalog image resource.
+static NSString * const ACImageNameHomeLight AC_SWIFT_PRIVATE = @"home_light";
+
+/// The "home_monster" asset catalog image resource.
+static NSString * const ACImageNameHomeMonster AC_SWIFT_PRIVATE = @"home_monster";
+
+/// The "home_orange" asset catalog image resource.
+static NSString * const ACImageNameHomeOrange AC_SWIFT_PRIVATE = @"home_orange";
+
+/// The "home_point" asset catalog image resource.
+static NSString * const ACImageNameHomePoint AC_SWIFT_PRIVATE = @"home_point";
+
+/// The "home_point1" asset catalog image resource.
+static NSString * const ACImageNameHomePoint1 AC_SWIFT_PRIVATE = @"home_point1";
+
+/// The "home_white" asset catalog image resource.
+static NSString * const ACImageNameHomeWhite AC_SWIFT_PRIVATE = @"home_white";
+
+/// The "homepage_off" asset catalog image resource.
+static NSString * const ACImageNameHomepageOff AC_SWIFT_PRIVATE = @"homepage_off";
+
+/// The "homepage_on" asset catalog image resource.
+static NSString * const ACImageNameHomepageOn AC_SWIFT_PRIVATE = @"homepage_on";
+
+/// The "leave_off" asset catalog image resource.
+static NSString * const ACImageNameLeaveOff AC_SWIFT_PRIVATE = @"leave_off";
+
+/// The "leave_on" asset catalog image resource.
+static NSString * const ACImageNameLeaveOn AC_SWIFT_PRIVATE = @"leave_on";
+
+/// The "model " asset catalog image resource.
+static NSString * const ACImageNameModel AC_SWIFT_PRIVATE = @"model ";
+
+/// The "play_off" asset catalog image resource.
+static NSString * const ACImageNamePlayOff AC_SWIFT_PRIVATE = @"play_off";
+
+/// The "play_on" asset catalog image resource.
+static NSString * const ACImageNamePlayOn AC_SWIFT_PRIVATE = @"play_on";
+
+/// The "previous page_off" asset catalog image resource.
+static NSString * const ACImageNamePreviousPageOff AC_SWIFT_PRIVATE = @"previous page_off";
+
+/// The "previous page_on" asset catalog image resource.
+static NSString * const ACImageNamePreviousPageOn AC_SWIFT_PRIVATE = @"previous page_on";
+
+/// The "single_off" asset catalog image resource.
+static NSString * const ACImageNameSingleOff AC_SWIFT_PRIVATE = @"single_off";
+
+/// The "single_on" asset catalog image resource.
+static NSString * const ACImageNameSingleOn AC_SWIFT_PRIVATE = @"single_on";
+
+/// The "square_gray" asset catalog image resource.
+static NSString * const ACImageNameSquareGray AC_SWIFT_PRIVATE = @"square_gray";
+
+/// The "square_orange border" asset catalog image resource.
+static NSString * const ACImageNameSquareOrangeBorder AC_SWIFT_PRIVATE = @"square_orange border";
+
+/// The "square_white" asset catalog image resource.
+static NSString * const ACImageNameSquareWhite AC_SWIFT_PRIVATE = @"square_white";
+
+/// The "square_yellow" asset catalog image resource.
+static NSString * const ACImageNameSquareYellow AC_SWIFT_PRIVATE = @"square_yellow";
+
+/// The "start game_off" asset catalog image resource.
+static NSString * const ACImageNameStartGameOff AC_SWIFT_PRIVATE = @"start game_off";
+
+/// The "start game_on" asset catalog image resource.
+static NSString * const ACImageNameStartGameOn AC_SWIFT_PRIVATE = @"start game_on";
+
+/// The "timer" asset catalog image resource.
+static NSString * const ACImageNameTimer AC_SWIFT_PRIVATE = @"timer";
+
+/// The "volume_off" asset catalog image resource.
+static NSString * const ACImageNameVolumeOff AC_SWIFT_PRIVATE = @"volume_off";
+
+/// The "volume_on" asset catalog image resource.
+static NSString * const ACImageNameVolumeOn AC_SWIFT_PRIVATE = @"volume_on";
+
+/// The "white_1" asset catalog image resource.
+static NSString * const ACImageNameWhite1 AC_SWIFT_PRIVATE = @"white_1";
+
+/// The "white_2" asset catalog image resource.
+static NSString * const ACImageNameWhite2 AC_SWIFT_PRIVATE = @"white_2";
+
+/// The "white_3" asset catalog image resource.
+static NSString * const ACImageNameWhite3 AC_SWIFT_PRIVATE = @"white_3";
+
+/// The "white_4" asset catalog image resource.
+static NSString * const ACImageNameWhite4 AC_SWIFT_PRIVATE = @"white_4";
+
+/// The "white_5" asset catalog image resource.
+static NSString * const ACImageNameWhite5 AC_SWIFT_PRIVATE = @"white_5";
+
+/// The "white_6" asset catalog image resource.
+static NSString * const ACImageNameWhite6 AC_SWIFT_PRIVATE = @"white_6";
+
+/// The "winner" asset catalog image resource.
+static NSString * const ACImageNameWinner AC_SWIFT_PRIVATE = @"winner";
+
+/// The "winnertop" asset catalog image resource.
+static NSString * const ACImageNameWinnertop AC_SWIFT_PRIVATE = @"winnertop";
+
+#warning The "大順" image asset name resolves to an empty symbol. Try renaming the asset.
+
+#warning The "小順" image asset name resolves to an empty symbol. Try renaming the asset.
+
+#warning The "瞌睡蟲" image asset name resolves to an empty symbol. Try renaming the asset.
+
+/// The "瞌睡蟲winner" asset catalog image resource.
+static NSString * const ACImageNamewinner AC_SWIFT_PRIVATE = @"瞌睡蟲winner";
+
+#warning The "萬事通" image asset name resolves to an empty symbol. Try renaming the asset.
+
+#warning The "萬事通winner" image asset name resolves to the symbol "ACImageNamewinner" which already exists. Try renaming the asset.
+
+#warning The "葫蘆" image asset name resolves to an empty symbol. Try renaming the asset.
+
+#warning The "開心鬼" image asset name resolves to an empty symbol. Try renaming the asset.
+
+#warning The "開心鬼winner" image asset name resolves to the symbol "ACImageNamewinner" which already exists. Try renaming the asset.
+
+#warning The "骰子台" image asset name resolves to an empty symbol. Try renaming the asset.
+
+#undef AC_SWIFT_PRIVATE
